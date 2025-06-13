da55e8326a6b36e7d2d74d572d5c8354438132a2
Elishacker <mahendaelisha60@gmail.com>
2025-06-13 15:18:10 +0300

    13/06/2025 Updates

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\EnrollmentController;
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EnrollmentController;
 use App\Http\Controllers\FaceAuthController;
 use App\Http\Controllers\FingerprintAuthController;
 
 Route::post('enroll-user', [EnrollmentController::class, 'store']);
 Route::post('/auth/face', [FaceAuthController::class, 'authenticate']);
 Route::post('/auth/fingerprint', [FingerprintAuthController::class, 'authenticate']);
+Route::get('/check-user', [EnrollUserController::class, 'checkUser']);
+Route::get('/user', [EnrollUserController::class, 'User']);
